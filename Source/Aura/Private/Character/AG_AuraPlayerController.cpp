25db6fd4d6f598c938c498a27dc6bfcd952707ab
moonabyss <d-v@ukr.net>
2024-05-01 22:12:46 +0200

    02.09. Aura Player Controller

--- Source/Aura/Private/Character/AG_AuraPlayerController.cpp
+++ Source/Aura/Private/Character/AG_AuraPlayerController.cpp
@@ -1,3 +1,32 @@
 // Aura Game, Copyright moonabyss. All Rights Reserved.
 
 #include "Character/AG_AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+AAG_AuraPlayerController::AAG_AuraPlayerController()
+{
+    bReplicates = true;
+}
+
+void AAG_AuraPlayerController::BeginPlay()
+{
+    Super::BeginPlay();
+
+    // Get Input Subsystem
+    UEnhancedInputLocalPlayerSubsystem* InputSubsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+    check(InputSubsystem);
+
+    // Set Mapping Context
+    check(AuraInputContext);
+    InputSubsystem->AddMappingContext(AuraInputContext, 0);
+
+    // Cursor
+    bShowMouseCursor = true;
+    DefaultMouseCursor = EMouseCursor::Default;
+
+    // Input mode
+    FInputModeGameAndUI InputModeData;
+    InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+    InputModeData.SetHideCursorDuringCapture(false);
+    SetInputMode(InputModeData);
+}
--- Source/Aura/Public/Character/AG_AuraPlayerController.h
+++ Source/Aura/Public/Character/AG_AuraPlayerController.h
@@ -7,8 +7,20 @@
 
 #include "AG_AuraPlayerController.generated.h"
 
+class UInputMappingContext;
+
 UCLASS()
 class AURA_API AAG_AuraPlayerController : public APlayerController
 {
     GENERATED_BODY()
+
+public:
+    AAG_AuraPlayerController();
+
+protected:
+    virtual void BeginPlay() override;
+
+private:
+    UPROPERTY()
+    TObjectPtr<UInputMappingContext> AuraInputContext;
 };
