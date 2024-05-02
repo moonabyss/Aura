2304ff6f9d830e7358891f8526ec25d4c5db2a60
moonabyss <d-v@ukr.net>
2024-05-02 14:45:14 +0200

    03.03. The Player State

---
+++ Source/Aura/Private/Character/Player/AG_AuraPlayerState.cpp
@@ -0,0 +1,8 @@
+// Aura Game, Copyright moonabyss. All Rights Reserved.
+
+#include "Character/Player/AG_AuraPlayerState.h"
+
+AAG_AuraPlayerState::AAG_AuraPlayerState()
+{
+    NetUpdateFrequency = 100.0f;
+}
---
+++ Source/Aura/Public/Character/Player/AG_AuraPlayerState.h
@@ -0,0 +1,17 @@
+// Aura Game, Copyright moonabyss. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+
+#include "AG_AuraPlayerState.generated.h"
+
+UCLASS()
+class AURA_API AAG_AuraPlayerState : public APlayerState
+{
+    GENERATED_BODY()
+
+public:
+    AAG_AuraPlayerState();
+};
